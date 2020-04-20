6dc51f09ef8352d26ed3b3aa7fd8de6c88679026
Annie Cypcar <annecypcar@gmail.com>
2020-04-19 20:52:05 -0400

    removes stray dir

--- constants/Queries.js
+++
@@ -1,9 +0,0 @@
-export const DATE_TIME = 'primary_release_date.gte=2016-01-01&primary_release_date.lte=2016-12-31';
-
-export const SORT_BY = 'sort_by';
-
-export const SORT_DIRECTION = { ASC: 'asc', DESC: 'desc' };
-
-export const SORT_OPTIONS = { POPULARITY: 'popularity' };
-
-export const LIMIT = { PAGE_MAX: 20 };
